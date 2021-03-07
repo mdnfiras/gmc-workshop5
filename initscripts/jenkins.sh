2d73bc02aa59acba608eb8472df13b22a9feed94
Firas Medini <mdnfiras@yahoo.com>
2021-03-07 11:25:37 +0100

    Update jenkins.sh

--- initscripts/jenkins.sh
+++ initscripts/jenkins.sh
@@ -9,3 +9,5 @@ cat $( docker volume inspect jenkins-volume | grep Mountpoint | cut -d '"' -f4 )
 useradd jenkins
 usermod -aG docker jenkins
 service docker restart
+docker rm myjenkins
+docker run -d --name myjenkins -p 8080:8080 -p 50000:50000 -v jenkins-volume:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock myjenkins:latest
