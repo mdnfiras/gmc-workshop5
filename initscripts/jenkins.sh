c6ff43386910326c18b6fe470c7a23f4be44c517
Firas Medini <mdnfiras@yahoo.com>
2021-03-07 14:15:55 +0100

    Update jenkins.sh

--- initscripts/jenkins.sh
+++ initscripts/jenkins.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
+useradd jenkins
+usermod -aG docker jenkins
+service docker restart
+sleep 5
 docker build -t myjenkins:latest -f jenkins_docker/Dockerfile jenkins_docker/
 docker volume create jenkins-volume
 docker run -d --name myjenkins -p 8080:8080 -p 50000:50000 -v jenkins-volume:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock myjenkins:latest
 while [[ -z "`curl localhost:8080 | grep Authentication | grep required`" ]]; do sleep 5; done
 echo "==========> Your jenkins administrative password is:"
 cat $( docker volume inspect jenkins-volume | grep Mountpoint | cut -d '"' -f4 )/secrets/initialAdminPassword
-useradd jenkins
-usermod -aG docker jenkins
-service docker restart
-docker rm myjenkins
-docker run -d --name myjenkins -p 8080:8080 -p 50000:50000 -v jenkins-volume:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock myjenkins:latest
